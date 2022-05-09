256e32c031f6756899066034742c9a5c78ac87be
Emily Ekaireb <eekaireb@ucsd.edu>
2022-05-08 18:32:10 -0700

    flip test data

--- fhdnn/sampling.py
+++ fhdnn/sampling.py
@@ -120,7 +120,8 @@ def mnist_iid(num_users):
 	dm.val_transform = transforms.Compose([
 		transforms.ToTensor(),
 		transforms.Normalize((0.1307,), (0.3081,)),
-		transforms.Lambda(lambda x: x.repeat(3, 1, 1) )
+		transforms.Lambda(lambda x: x.repeat(3, 1, 1) ),
+		transforms.RandomVerticalFlip(1) 
 	])
 
 

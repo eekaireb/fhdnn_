3cc317fa533b87ebaed2a10d1c7edf2277a2f478
Emily Ekaireb <eekaireb@ucsd.edu>
2022-05-28 19:31:26 -0700

    update

--- fhdnn/mnistDataModule.py
+++ fhdnn/mnistDataModule.py
@@ -89,7 +89,9 @@ class MnistData(pl.LightningDataModule):
             print(len(train_subset))
             self.train_sampler = SeqSampler(train_subset, self.blend_ratio, self.n_concurrent_classes) \
                 if self.training_data_type == 'sequential' else None
-            train_loader = DataLoader(train_subset, batch_size=self.batch_size, shuffle=(self.train_sampler is None),
+            #train_loader = DataLoader(train_subset, batch_size=self.batch_size, shuffle=(self.train_sampler is None),
+            #                num_workers=self.workers, pin_memory=True, sampler=self.train_sampler)
+            train_loader = DataLoader(dataset, batch_size=self.batch_size, shuffle=(self.train_sampler is None),
                             num_workers=self.workers, pin_memory=True, sampler=self.train_sampler)
             return train_loader
             #
